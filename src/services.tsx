7a344a98e0703f435a291ee7dbec324cb809e153
pranjal1642 <pranjalgupta1642@gmail.com>
2024-04-06 17:53:55 +0530

    changes

--- src/services.tsx
+++ src/services.tsx
@@ -1,26 +1,31 @@
 import CryptoJS from 'crypto-js';
 
-export const decryptData = (req: any,) => {
-	if (true) {
+export const decryptData = (req: any) => {
+	try {
 		const decryptData = CryptoJS.AES.decrypt(
 			req.body.data,
-			"LeW79Rhdjh67ZqOMvYKZAwr5kE43vwz",
+			'LeW79Rhdjh67ZqOMvYKZAwr5kE43vwz',
 		);
 		const stringData = decryptData.toString(CryptoJS.enc.Utf8);
 		req.body = JSON.parse(stringData);
+	} catch (error) {
+		console.error('Error decrypting data:', error);
+		// Handle the error as per your application's requirements
 	}
 };
 
 export const encryptData = (data: any) => {
-	if (true) {
-		console.log(data, "PPPPPPPPPPP");
+	try {
+		console.log(data, 'PPPPPPPPPPP');
 		const stringData = JSON.stringify(data);
 		const encryptData = CryptoJS.AES.encrypt(
 			stringData,
-			"LeW79Rhdjh67ZqOMvYKZAwr5kE43vwz",
+			'LeW79Rhdjh67ZqOMvYKZAwr5kE43vwz',
 		).toString();
 		return encryptData;
-	} else {
-		return data;
+	} catch (error) {
+		console.error('Error encrypting data:', error);
+		// Handle the error as per your application's requirements
+		return null; // Return null or handle the error accordingly
 	}
 };
