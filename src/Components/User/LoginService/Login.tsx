9d46cb7a808e6f39f3c36683a3b22e77b9a6b9cc
pranjal1642 <pranjalgupta1642@gmail.com>
2024-04-17 21:17:01 +0530

    changes

--- src/Components/User/LoginService/Login.tsx
+++ src/Components/User/LoginService/Login.tsx
@@ -1,67 +1,72 @@
-import { useForm } from "react-hook-form";
-import { LoginApi } from "../../../apiServices/apiFetch";
+import { useForm } from 'react-hook-form';
+import { LoginApi } from '../../../apiServices/apiFetch';
+import './Login.scss';
 
 const Login = () => {
-  const { register, handleSubmit, formState: { errors }, } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 
-  // login function
-  const login = async (e: any) => {
-    try {
-      const loginPayload = { email: e?.email, password: e?.password };
-      const res = LoginApi(loginPayload);
-      console.log(res, "LOGIN");
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
+	// login function
+	const login = async (e: any) => {
+		try {
+			const loginPayload = { email: e?.email, password: e?.password };
+			const res = LoginApi(loginPayload);
+			console.log(res, 'LOGIN');
+		} catch (error) {
+			console.error('Error:', error);
+		}
+	};
 
-  // onsubmit function
-  const onSubmit = (data: any) => {
-    login(data);
-  };
+	// onsubmit function
+	const onSubmit = (data: any) => {
+		login(data);
+	};
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <div className="login">
-            <div className="login-header">
-              <h2>HrPortal Login</h2>
-            </div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div>
-                <label>Email:</label>
-                <input
-                  className="form-control"
-                  placeholder="Email"
-                  type="email"
-                  {...register("email", { required: "Email is required" })}
-                />
-                {errors.email && <p>Emial Is Required</p>}
-              </div>
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-6">
+					<div className="login">
+						<div className="login-header">
+							<h2>HrPortal Login</h2>
+						</div>
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<div>
+								<label>Email:</label>
+								<input
+									className="form-control"
+									placeholder="Email"
+									type="email"
+									{...register('email', { required: 'Email is required' })}
+								/>
+								{errors.email && <p>Emial Is Required</p>}
+							</div>
 
-              <div>
-                <label>Password:</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="**********"
-                  {...register("password", {
-                    required: "Password is required",
-                  })}
-                />
-                {errors.password && <p>Password is required</p>}
-              </div>
+							<div>
+								<label>Password:</label>
+								<input
+									type="password"
+									className="form-control"
+									placeholder="**********"
+									{...register('password', {
+										required: 'Password is required',
+									})}
+								/>
+								{errors.password && <p>Password is required</p>}
+							</div>
 
-              <button className="btn btn-primary mt-4" type="submit">
-                Login
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+							<button className="btn btn-primary mt-4" type="submit">
+								Login
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
--- src/apiServices/apiFetch.ts
+++ src/apiServices/apiFetch.ts
@@ -29,3 +29,4 @@ export const Signup = (data: any) => {
     console.log("SIGNUP ERROR");
   }
 };
+
